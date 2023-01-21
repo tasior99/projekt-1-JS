a5126d4c5eb8a254319b61424b944ecea86a67a8
Krzysztof Kantor <krzysio.kantor@gmail.com>
2023-01-21 22:46:51 +0100

    poprawka

--- main.js
+++ main.js
@@ -16,46 +16,72 @@ const expenseTotal = document.querySelector("#expensesValue");
 const incomeTotal = document.querySelector("#incomesValue");
 const income = "income";
 const expense = "expense";
+const incomeTotalMap = {};
+const expenseTotalMap = {};
+let budgetValue = 0;
 let incomeTotalValue = 0;
 let expenseTotalValue = 0;
-let budgetValue = 0;
 function sanitizeString(str) {
   str = str.replace(/[^a-z0-9áéíóúñü \.,_-]/gim, "");
   return str.trim();
 }
+let guid = () => {
+  let s4 = () => {
+    return Math.floor((1 + Math.random()) * 0x10000)
+      .toString(16)
+      .substring(1);
+  };
+  return s4() + "-" + s4();
+};
 const addIncome = (event) => {
   event.preventDefault();
   const incomeTitle = sanitizeString(incomeTitleInput.value);
   const incomeValue = Number(incomeValueInput.value);
-  const incomeElement = createBudgetElement(incomeTitle, incomeValue, income);
+  const budgetId = guid();
+  const incomeElement = createBudgetElement(
+    incomeTitle,
+    incomeValue,
+    income,
+    budgetId
+  );
 
   incomeList.appendChild(incomeElement);
-  modifyTotalIncome(incomeValue);
+  incomeTotalMap[budgetId] = incomeValue;
+  updateTotalIncome();
 };
-const modifyTotalIncome = (value) => {
-  incomeTotalValue = incomeTotalValue + value;
+const updateTotalIncome = () => {
+  incomeTotalValue = 0;
+  for (const incomeKey in incomeTotalMap) {
+    incomeTotalValue = incomeTotalValue + incomeTotalMap[incomeKey];
+  }
   incomeTotal.innerText = incomeTotalValue;
-  addDiffrence(incomeTotalValue);
+  updateDiffrence();
 };
 const addExpense = (event) => {
   event.preventDefault();
   const expenseTitle = sanitizeString(expenseTitleInput.value);
   const expenseValue = Number(expenseValueInput.value);
+  const budgetId = guid();
   const expenseElement = createBudgetElement(
     expenseTitle,
     expenseValue,
-    expense
+    expense,
+    budgetId
   );
   expenseList.appendChild(expenseElement);
-  modifyTotalExpense(expenseValue);
+  expenseTotalMap[budgetId] = expenseValue;
+  updateTotalExpense();
 };
 
-const modifyTotalExpense = (value) => {
-  expenseTotalValue = expenseTotalValue + value;
+const updateTotalExpense = () => {
+  expenseTotalValue = 0;
+  for (const expenseKey in expenseTotalMap) {
+    expenseTotalValue = expenseTotalValue + expenseTotalMap[expenseKey];
+  }
   expenseTotal.innerText = expenseTotalValue;
-  addDiffrence(expenseTotalValue);
+  updateDiffrence();
 };
-const addDiffrence = () => {
+const updateDiffrence = () => {
   budgetValue = incomeTotalValue - expenseTotalValue;
   budgetDiffrence.innerText = budgetValue;
   budgetStatus(budgetValue);
@@ -75,42 +101,46 @@ function createBudgetText(title, value) {
   budgetText.innerHTML = `<span class="title">${title}</span>: <span class="value">${value}</span> PLN`;
   return budgetText;
 }
-function modifyBudgetElement(budgetElement, budgetValueType) {
+function modifyBudgetElement(budgetElement, budgetValueType, budgetId) {
   const budgetTitle = window.prompt("Title:");
-  const budgetAmount = window.prompt("Amount:");
+  const budgetAmount = Number(window.prompt("Amount:"));
   const budgetTitleElement = budgetElement.querySelector(".title");
   const budgetValueElement = budgetElement.querySelector(".value");
-  const oldBudgetValue = Number(budgetValueElement.innerText);
+
   if (budgetValueType === income) {
-    modifyTotalIncome(budgetAmount - oldBudgetValue);
+    incomeTotalMap[budgetId] = budgetAmount;
+    updateTotalIncome();
   } else if (budgetValueType === expense) {
-    modifyTotalExpense(budgetAmount - oldBudgetValue);
+    expenseTotalMap[budgetId] = budgetAmount;
+    updateTotalExpense();
   }
   budgetTitleElement.innerText = budgetTitle;
   budgetValueElement.innerText = budgetAmount;
 }
-function removeBudgetElement(budgetElement, budgetValueType, value) {
+function removeBudgetElement(budgetElement, budgetValueType, budgetId) {
   if (budgetValueType === income) {
-    modifyTotalIncome(value * -1);
+    delete incomeTotalMap[budgetId];
+    updateTotalIncome();
   } else if (budgetValueType === expense) {
-    modifyTotalExpense(value * -1);
+    delete expenseTotalMap[budgetId];
+    updateTotalExpense();
   }
   budgetElement.remove();
 }
 
-const createBudgetElement = (title, value, budgetValueType) => {
+const createBudgetElement = (title, value, budgetValueType, budgetId) => {
   const budgetElement = document.createElement("div");
   budgetElement.className = "budgetElement";
   budgetElement.appendChild(createBudgetText(title, value));
   const editButton = document.createElement("button");
   editButton.innerText = "edit";
   editButton.addEventListener("click", () => {
-    modifyBudgetElement(budgetElement, budgetValueType);
+    modifyBudgetElement(budgetElement, budgetValueType, budgetId);
   });
   const removeButton = document.createElement("button");
   removeButton.innerText = "remove";
   removeButton.addEventListener("click", () => {
-    removeBudgetElement(budgetElement, budgetValueType, value);
+    removeBudgetElement(budgetElement, budgetValueType, budgetId);
   });
   budgetElement.appendChild(editButton);
   budgetElement.appendChild(removeButton);
